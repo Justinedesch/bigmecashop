c690beeea6b9dbf31b518727e16a5fbbaaaf01e9
Justine.D <justine.deschamps@le-campus-numerique.fr>
2023-05-31 13:20:06 +0200

    estion panier avec tableau

--- cart.php
+++ cart.php
@@ -1,6 +1,7 @@
 <?php
     include "functions.php";
     include "produits.php";
+session_start();
 ?>
 
 
@@ -11,11 +12,26 @@
 
 <p>
     <?php
-    foreach ($_POST as $IDproduit=>$nomDuProduit) {
-    echo "<li> $IDproduit :  $nomDuProduit </li>" ;
+
+
+    if(isset($_POST["id_produit"]) AND (isset($_POST["Quantité"])))
+    {
+     $_SESSION = ["panier"=> [
+         ["id_produit"=>$_POST["id_produit"],"Quantité"=>$_POST["Quantité"]],
+         ["id_produit"=>$_POST["id_produit"],"Quantité"=>$_POST["Quantité"]]
+     ]];
     }
 
-    ?>
+foreach ($_SESSION as $item) {
+    echo "identifiant produit : " . $_SESSION ["panier"][0]["id_produit"];
+    echo "<br>";
 
+    echo "Quantité : " . $_SESSION ["panier"][0]["Quantité"];
+    echo "Prix unitaire HT :" .
+        get_produits() [$_SESSION ["panier"][0]["id_produit"]]["prixHT"];
+    echo "Prix Total :" . get_produits() [$_SESSION ["panier"][0]["id_produit"]]["prixHT"] * $_SESSION["panier"][0]["Quantité"];
+}
+    ?>
 </p>
+
 
--- catalogue.php
+++ catalogue.php
@@ -2,6 +2,7 @@
         include ("header.php");
         include "functions.php";
         include "produits.php";
+    session_start();
     ?>
 
     <?php
