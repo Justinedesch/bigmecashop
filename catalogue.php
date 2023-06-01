7f387584225ab857d9053d43a57f06c2d8fbb49a
Justine.D <justine.deschamps@le-campus-numerique.fr>
2023-06-01 16:22:11 +0200

    dernier commit php

--- catalogue.php
+++ catalogue.php
@@ -1,71 +1,54 @@
-    <?php
-        include ("header.php");
-        include "functions.php";
-        include "produits.php";
-    session_start();
-    ?>
+<?php
+include("header.php");
+include "functions.php";
+include "produits.php";
+session_start();
+?>
 
-    <?php
-         $produits = get_produits();
+<?php
 
-    //    foreach ($produits as &$produit) {
-    //        $produit['prixTTC'] = calculPrixTTC($produit['prixHT']);
-    //        $produit['prixPromo'] = calculPrixSolde($produit['prixHT'], $produit['promotion']);
-    //    }
-    //    echo '<pre>';
-    //    print_r($produits);
-    //    echo '/<pre>';
-
-        foreach ($produits as $key => $produit) :
+$produits = get_produits();
+foreach ($produits as $key => $produit) :
     ?>
     <div class="produit">
-
-       <h1><?= $produit['titre']?> </h1>
-       <img src= "<?= $produit['photo'] ?>" />
-       <p><?= $produit['prixHT'] ?> </p>
-
-       <p>
-
-         <button <?php if ($produit ['disponnibilite'] == false) : ?>disabled <?php endif; ?>>
-           Acheter
-         </button>
-
+        <h1><?= $produit['titre'] ?> </h1>
+        <img src="<?= $produit['photo'] ?>"/>
+        <p><?= $produit['prixHT'] ?> </p>
+        <p>
+            <button <?php if ($produit ['disponnibilite'] == false) : ?>disabled <?php endif; ?>>
+                Acheter
+            </button>
         </p>
 
-            <div class="tarifs">
+        <div class="tarifs">
+            Prix HT = <?php echo $produit['prixHT']; ?> €
+            Prix TTC = <?php {
+                echo calculPrixTTC($produit ['prixHT']);
+            } ?> €
+            Prix Soldé = <?php if ($produit['promotion'] > 0) {
+                echo calculPrixSolde($produit['prixHT'], $produit['promotion']);
+            } ?> €
 
-             Prix HT = <?php echo $produit['prixHT'];?> €
-             Prix TTC = <?php { echo calculPrixTTC($produit ['prixHT']);} ?> €
-             Prix Soldé = <?php if ($produit['promotion'] >0) { echo calculPrixSolde($produit['prixHT'],$produit['promotion']);} ?> €
+        </div>
 
-            </div>
+        <div class="formulaire">
 
-            <div class ="formulaire">
-
-                <form name="panier" action="cart.php" method="POST">
-                    <label for="Quantité"> Quantité : </label>
-                    <input type="hidden" name="id_produit" value= "<?php echo $key; ?>" />
-                    <input type="number" name="Quantité"/>
-                    <input type="submit" value="Ajouter au panier"/>
-                </form>
-
-                <hr>
-
-            </div>
-
+            <form name="panier" action="cart.php" method="POST">
+                <label for="Quantité"> Quantité : </label>
+                <input type="hidden" name="id_produit" value="<?php echo $key; ?>"/>
+                <input type="number" name="Quantité"/>
+                <input type="submit" value="Ajouter au panier"/>
+            </form>
+            <hr>
+        </div>
     </div>
 
-
-    <?php
-        endforeach;
-    ?>
-
-
-
-
-    <?php
-        include ("footer.php");
-    ?>
+<?php
+endforeach;
+?>
+<?php
+include("footer.php");
+?>
 
 
 
