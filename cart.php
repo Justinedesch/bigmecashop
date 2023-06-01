0806fc1a1be29a016a0be1450b43584feea7af97
Justine.D <justine.deschamps@le-campus-numerique.fr>
2023-06-01 09:08:13 +0200

    boutique avec panier fonctionnel complet

--- cart.php
+++ cart.php
@@ -4,6 +4,31 @@ include "produits.php";
 session_start();
 ?>
 
+<!--LOGIQUE-->
+
+<?php
+
+//Vider mon panier
+
+if (isset ($_POST ["emptycart"])) {
+    unset($_SESSION ["panier"]);
+}
+
+// Supprimer un article
+if (isset ($_POST ["delete_Article"])) {
+    $_SESSION['panier'] [$_POST["id_produit"]] ["Quantité"]--;
+}
+
+// Ajouter un article
+if (isset ($_POST ["add_Article"])) {
+    $_SESSION['panier'][$_POST["id_produit"]] ["Quantité"]++;
+}
+
+var_dump($_SESSION);
+?>
+
+
+<!--AFFICHAGE-->
 
 <h1>Mon Panier :</h1>
 
@@ -19,7 +44,7 @@ session_start();
 
     if (isset($_POST["id_produit"]) and (isset($_POST["Quantité"]))) {
 
-        $_SESSION ["panier"][] = ["id_produit" => $_POST["id_produit"], "Quantité" => $_POST["Quantité"]];
+        $_SESSION ["panier"] [$_POST["id_produit"]] = ["id_produit" => $_POST["id_produit"], "Quantité" => $_POST["Quantité"]];
     }
     //=> Ma condition ci-dessus indique "si mon formulaire "POST" contient un ID produit et une quantité, je les ajoutent à mon tableau SESSION
 
@@ -29,36 +54,68 @@ session_start();
     // Je parcours mon tableau de produits. Pour chaque produit j'affiche les éléments :
 
     foreach ($_SESSION["panier"] as $item) {
-        echo "Nom du produit : " . $item ["id_produit"];
-        echo "<br>";
+    echo "Nom du produit : " . $item ["id_produit"];
+    echo "<br>";
 
-        echo "Quantité : " . $item ["Quantité"];
-        echo "<br>";
+    echo "Quantité : " . $item ["Quantité"];
+    echo "<br>";
 
-        echo "Prix unitaire HT = " .
-            get_produits() [$item ["id_produit"]]["prixHT"];
-        echo "<br>";
+    echo "Prix unitaire HT = " .
+        get_produits() [$item ["id_produit"]]["prixHT"];
+    echo "<br>";
 
-        // Pour mon total je crée une variable total article qui reprends ma fonction de calcul prix unitaire, et je la mutliplie ap rle nb d'articles
-        $TotalArticle = get_produits() [$item["id_produit"]]["prixHT"] * $item ["Quantité"];
-        echo "Prix Total = " . $TotalArticle;
-        echo "<br>";
+    // Pour mon total je crée une variable total article qui reprends ma fonction de calcul prix unitaire, et je la mutliplie ap rle nb d'articles
+    $TotalArticle = get_produits() [$item["id_produit"]]["prixHT"] * $item ["Quantité"];
+    echo "Prix Total = " . $TotalArticle;
+    echo "<br>";
 
-        //Pour mon total panier, j'initialis eun total à 0 avant ma boucle foreach. Puis je fait mon calcul totalpanier = le total actuel + le total d'articles
-        $TotalPanier = $TotalPanier + $TotalArticle;
-        echo "<br>";
+    //Pour mon total panier, j'initialis eun total à 0 avant ma boucle foreach. Puis je fait mon calcul totalpanier = le total actuel + le total d'articles
+    $TotalPanier = $TotalPanier + $TotalArticle;
+    echo "<br>";
+    ?>
 
-    }
+    <!-- Supprimer un article -->
+    <form name="Supprimer_article" action="cart.php" method="POST">
+        <input type="hidden" name="id_produit" value="<?php echo $item["id_produit"]; ?>"/>
+        <input type="hidden" name="delete_Article" value="1">
+        <input type="submit" value="Retirer un article"/>
+    </form>
 
-    //J'imprime bien après la boucle pour ne pas qu'il m'affiche a chauqeà rticle le total
-    echo "TOTAL PANIER = " . $TotalPanier; ?> €
-    <br>
-    <br>
-    <button>
-        Vider le Panier
-    </button>
 
+<!--  // Ajouter un article -->
+    <form name="Ajouter_article" action="cart.php" method="POST">
+        <input type="hidden" name="id_produit" value="<?php echo $item["id_produit"]; ?>"/>
+        <input type="hidden" name="add_Article" value="1">
+        <input type="submit" value="Ajouter un article"/>
+    </form>
+
+<?php
+
+
+}
+
+
+//J'imprime bien après la boucle pour ne pas qu'il m'affiche a chauqeà rticle le total
+echo "TOTAL PANIER = " . $TotalPanier; ?> €
+<br>
+<br>
 
 </p>
+
+
+<div class="Forumulaires pour panier">
+
+    <!-- Vider mon panier -->
+    
+    <form name="Vider_panier" action="cart.php" method="POST">
+        <input type="hidden" name="id_produit" value="<?php echo $item; ?>"/>
+        <input type="hidden" name="emptycart" value="1">
+        <input type="submit" value="Vider le Panier"/>
+    </form>
+
+
+</div>
+
+
 
 
--- save.php
+++ save.php
@@ -3,23 +3,25 @@
 echo "votre panier est vide"; ?>
 
 
-<form name="panier" action="cart.php" method="POST">
-    <input type="hidden" name="emptycart" value="1">
-    <input type="submit" value="Vider le Panier"/>
-</form>
+    <form name="panier" action="cart.php" method="POST">
+        <input type="hidden" name="emptycart" value="1">
+        <input type="submit" value="Vider le Panier"/>
+    </form>
 
-<!--    //Supprimer article-->
-<!--    --><?php //unset($_SESSION[$item["#"]]);
-//session_start();
-//
-//function AjouterQte(){
-//  $_SESSION['panier'][$_POST["id_produit"]]++;
-//  header("location:index.php?page=panier");
-//}
-//
-//function EnleverQte(){
-//  $_SESSION['panier'][$_POST["id_produit"]]--;
-//  header("location:index.php?page=panier");
-//}
+    //Supprimer article
+<?php unset($_SESSION[$item["#"]]);
+session_start();
+
+function AjouterQte()
+{
+    $_SESSION['panier'][$_POST["id_produit"]]++;
+
+}
+
+function EnleverQte()
+{
+    $_SESSION['panier'][$_POST["id_produit"]]--;
+    header("location:catalogue.php");
+}
 
 
